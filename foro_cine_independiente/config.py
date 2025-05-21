d318aa4cb86e761f46ef840db1ed0792b720b811
Tadeo Vardánega <tadeo.vardanega@gmail.com>
2025-05-20 22:49:00 -0300

    Delete foro_cine_independiente directory

--- foro_cine_independiente/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'esta-es-una-clave-secreta-muy-segura'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'foro.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
